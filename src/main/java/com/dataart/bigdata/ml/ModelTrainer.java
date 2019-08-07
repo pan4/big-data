6c98d4adf34fe19db78b52505243b702112139b9
Alexander Panchenko <Alexander.Panchenko@dataart.com>
2019-08-07 13:47:05 +0300

    upd posts without tags

--- src/main/java/com/dataart/bigdata/ml/ModelTrainer.java
+++ src/main/java/com/dataart/bigdata/ml/ModelTrainer.java
@@ -32,6 +32,7 @@ public class ModelTrainer {
         Dataset<PostTags> tagDataset = df.select("_Id", "_Tags").flatMap((Row row) -> {
             List<PostTags> list = new ArrayList<>();
             if (row.getString(1) == null) {
+                list.add(new PostTags((int) row.getLong(0), null));
                 return list.iterator();
             }
             String[] tags = row.getString(1).split(">");
@@ -45,6 +46,7 @@ public class ModelTrainer {
         }, postTagEncoder);
 
         tagDataset.select("tagName")
+                .where(tagDataset.col("tagName").isNotNull())
                 .distinct()
                 .createOrReplaceTempView("df");
         Dataset<Row> tagNames = spark.sql("select row_number() over (order by 'tagName') as tagId, * from df");
@@ -52,9 +54,13 @@ public class ModelTrainer {
                 .csv(INT_TO_TAG_PATH);
 
         Encoder<Rating> ratingEncoder = Encoders.bean(Rating.class);
-        Dataset<Rating> rating = tagDataset.join(tagNames, tagDataset.col("tagName").equalTo(tagNames.col("tagName")))
+        Dataset<Rating> rating = tagDataset.join(tagNames, tagDataset.col("tagName").equalTo(tagNames.col("tagName")),"left")
                 .select("postId", "tagId")
-                .map((Row row) -> new Rating(row.getInt(0), row.getInt(1), (double) 1), ratingEncoder);
+                .map((Row row) -> {
+                    int productId = row.get(1) == null ? 0 : (int) row.get(1);
+                    double rat = productId == 0 ? 0 : 1;
+                    return new Rating(row.getInt(0), productId, rat);
+                    }, ratingEncoder);
 
         MatrixFactorizationModel model = ALS.trainImplicit(rating.rdd(), 30, 10);
         model.save(spark.sparkContext(), MODEL_PATH);
